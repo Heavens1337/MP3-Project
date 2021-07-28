748b15c37e4216f6cd90c21a2cb42390729a8924
Heavens <heyitscaelam@gmail.com>
2021-07-28 11:48:12 -0230

    Gonna try and get autoplay done in a very long and difficult way. I love programming :)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from guizero import App, PushButton, ListBox, Slider, Text, CheckBox
+from guizero import App, PushButton, ListBox, Slider, Text
 
 
 pygame.mixer.init()
@@ -60,12 +60,50 @@ def music_select():
         pygame.mixer.music.load('/Users/calemthomas/music files/Nero Forte.mp3')
 
 
+def queue_func():
+    if song_list.value == "Venom of Venus":
+        pygame.mixer.music.queue('/Users/calemthomas/music files/Armata Strigoi.mp3')
+
+    elif song_list.value == "Armata Strigoi":
+        pygame.mixer.music.queue('/Users/calemthomas/music files/Army of the Night.mp3')
+
+    elif song_list.value == "Army of the Night":
+        pygame.mixer.music.queue('/Users/calemthomas/music files/To Hell and Back.mp3')
+
+    elif song_list.value == "To Hell and Back":
+        pygame.mixer.music.queue('/Users/calemthomas/music files/Shiroyama.mp3')
+
+    elif song_list.value == "Shiroyama":
+        pygame.mixer.music.queue('/Users/calemthomas/music files/The Last Stand.mp3')
+
+    elif song_list.value == "The Last Stand":
+        pygame.mixer.music.queue('/Users/calemthomas/music files/Shepherd of Fire.mp3')
+
+    elif song_list.value == "Shepherd of Fire":
+        pygame.mixer.music.queue('/Users/calemthomas/music files/Bat Country.mp3')
+
+    elif song_list.value == "Bat Country":
+        pygame.mixer.music.queue('/Users/calemthomas/music files/Unholy Confessions.mp3')
+
+    elif song_list.value == "Unholy Confessions":
+        pygame.mixer.music.queue('/Users/calemthomas/music files/All Out Life.mp3')
+
+    elif song_list.value == "All Out Life":
+        pygame.mixer.music.queue('/Users/calemthomas/music files/Before I Forget.mp3')
+
+    elif song_list.value == "Before I Forget":
+        pygame.mixer.music.queue('/Users/calemthomas/music files/Nero Forte.mp3')
+
+    elif song_list.value == "Nero Forte":
+        pygame.mixer.music.queue('/Users/calemthomas/music files/Venom of Venus.mp3')
+
+
 interface = App(title="MP3 Player", height=500, width=400, layout="auto")
 
 song_list = ListBox(interface, items=[
-    "Army of the Night",
-    "Armata Strigoi",
     "Venom of Venus",
+    "Armata Strigoi",
+    "Army of the Night",
     "To Hell and Back",
     "Shiroyama",
     "The Last Stand",
@@ -81,9 +119,9 @@ song_list = ListBox(interface, items=[
 play_button = PushButton(interface, command=play_track, text="Play from start")
 pause_button = PushButton(interface, command=pause_track, text="Pause")
 resume_button = PushButton(interface, command=resume_track, text="Resume")
-volume_slider = Slider(interface, start=100, end=1, horizontal=False, command=volume_alter, align="left")
-slider_title = Text(interface, text="Volume Slider", align="left")
-autoplay_bool = CheckBox(interface, text="Autoplay tracks?", align="right")
+volume_slider = Slider(interface, start=1, end=100, horizontal=True, command=volume_alter, align="bottom")
+slider_title = Text(interface, text="Volume Slider", align="bottom")
+queue_next_song = PushButton(interface, text="Queue Next", command=queue_func)
 
 
 interface.display()
